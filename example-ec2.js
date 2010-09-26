c7be3bfd98d231fea81eff71b571b19fb550877a
Mirko Kiefer <mirko.kiefer@arcor.de>
2010-09-26 03:56:05 +0200

    changed readme

--- example-ec2.js
+++ example-ec2.js
@@ -1,6 +1,6 @@
-var swirl = require("./lib/node-aws");
+var aws = require("./lib/node-aws");
 
-ec2 = swirl.createEC2Client(yourAccessKeyId, yourSecretAccessKey);
+ec2 = aws.createEC2Client(yourAccessKeyId, yourSecretAccessKey);
 
 ec2.call("DescribeInstances", {}, function(result) {
   console.log(JSON.stringify(result));
--- example-prod-adv.js
+++ example-prod-adv.js
@@ -1,6 +1,6 @@
-var swirl = require("./lib/node-aws");
+var aws = require("./lib/node-aws");
 
-prodAdv = swirl.createProdAdvClient(yourAccessKeyId, yourSecretAccessKey, yourAssociateTag);
+prodAdv = aws.createProdAdvClient(yourAccessKeyId, yourSecretAccessKey, yourAssociateTag);
 
 prodAdv.call("ItemSearch", {SearchIndex: "Books", Keywords: "Javascript"}, function(result) {
   console.log(JSON.stringify(result));
