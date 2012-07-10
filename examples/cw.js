1c5c758a887e6b712b5ef3061439a7b916f7139e
Mirko Kiefer <mail@mirkokiefer.com>
2012-07-10 18:24:15 +0200

    fix cloudwatch example

--- examples/cw.js
+++ examples/cw.js
@@ -2,6 +2,6 @@ var aws = require("../lib/aws");
 
 cw = aws.createCWClient(yourAccessKeyId, yourSecretAccessKey);
 
-cw.call("ListMetrics", {}, function(result) {
+cw.call("ListMetrics", {}, function(err, result) {
   console.log(JSON.stringify(result));
 })
